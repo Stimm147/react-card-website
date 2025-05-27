c62962d1b64326aeeb7aedad22ef5e206bbb420f
Stimm147 <michalczewski0@gmail.com>
2025-05-27 23:44:43 +0200

    corrections

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react";
 
 const repoName = "react-card-website";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: `/${repoName}/`,
-  build: {
-    outDir: "../dist",
-  },
 });

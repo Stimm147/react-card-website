f9582fccd45388b303a2fdb97a20aded2be7acb3
Stimm147 <michalczewski0@gmail.com>
2025-05-27 23:23:40 +0200

    redundant node packages removed

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,7 +6,7 @@ const repoName = "react-card-website";
 export default defineConfig({
   plugins: [react()],
   base: `/${repoName}/`,
-  // build: {
-  //   outDir: "../dist",
-  // },
+  build: {
+    outDir: "../dist",
+  },
 });
